37dd07d9c535990d8cb586ea07acf46936c63a59
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-08-28 19:49:19 +0900

    Importing Translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,15 @@
 export const translations = {
-  kr: {
-    "About Us": "어바웃"
-  }
+  ko: {
+    "About Us": "INSTAGRAM 정보",
+    Support: "지원",
+    Blog: "홍보",
+    Press: "센터",
+    API: "",
+    Jobs: "채용정보",
+    Privacy: "개인정보처리방침",
+    Terms: "약관",
+    Directory: "디렉터리",
+    Language: "언어"
+  },
+  options: {}
 };
