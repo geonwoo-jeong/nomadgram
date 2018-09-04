43cd8265c42430e3d25e7cafb3e3db30e34e6bc6
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-09-04 21:42:46 +0900

    FeedPhoto Component pt. Four

---
+++ frontend/src/components/CommentBox/presenter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
+
+const CommentBox = (props, context) => (
+  <form>
+    <textarea placeholder={context.t("Add a comment...")} />
+  </form>
+);
+
+CommentBox.contextTypes = {
+  t: PropTypes.func.isRequired
+};
+
+CommentBox.propTypes = {};
+
+export default CommentBox;
