d06404df2ee3ef78ec3890fc2396ce41f0b87b7a
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-09-06 21:04:34 +0900

    Getting and setting the Likes

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -4,6 +4,7 @@
 
 const SAVE_TOKEN = "SAVE_TOKEN";
 const LOGOUT = "LOGOUT";
+const SET_USER_LIST = "SET_USER_LIST";
 
 // action creators
 
@@ -17,6 +18,13 @@ function saveToken(token) {
 function logout() {
   return {
     type: LOGOUT
+  };
+}
+
+function setUserList(userList) {
+  return {
+    type: SET_USER_LIST,
+    userList
   };
 }
 
@@ -69,6 +77,28 @@ function createAccount(username, password, email, name) {
   };
 }
 
+function getPhotoLikes(photoId) {
+  return (dispatch, getState) => {
+    const {
+      user: { token }
+    } = getState();
+    fetch(`/images/${photoId}/likes`, {
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(logout());
+        }
+        return response.json();
+      })
+      .then(json => {
+        dispatch(setUserList(json));
+      });
+  };
+}
+
 // initial state
 
 const initialState = {
@@ -106,6 +136,8 @@ function reducer(state = initialState, action) {
       return applySetToken(state, action);
     case LOGOUT:
       return applyLogout(state, action);
+    case SET_USER_LIST:
+      return applySetUserList(state, action);
     default:
       return state;
   }
@@ -130,13 +162,22 @@ function applyLogout(state, action) {
   };
 }
 
+function applySetUserList(state, action) {
+  const { userList } = action;
+  return {
+    ...this.state,
+    userList
+  };
+}
+
 // exports
 
 const actionCreators = {
   facebookLogin,
   usernameLogin,
   createAccount,
-  logout
+  logout,
+  getPhotoLikes
 };
 
 export { actionCreators };
