da1bfb08d04233dc498ff9c0865be87b0818224b
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-09-10 21:38:38 +0900

    Search Component pt. One

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,3 +1,16 @@
 import { connect } from "react-redux";
 import Container from "./container";
-export default connect()(Container);
+import { push } from "react-router-redux";
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    goToSearch: searchTerm => {
+      dispatch(push(`/search/${searchTerm}`));
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Container);
